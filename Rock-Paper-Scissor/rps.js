92111cb5ae12ff9ca0990304e6acda4d6c266399
hemalatha <thutahemalatha@gmail.com>
2026-02-08 20:10:05 +0530

    rock paper scissor css added

--- Rock-Paper-Scissor/rps.js
+++ Rock-Paper-Scissor/rps.js
@@ -11,65 +11,62 @@ function playGame(playerMove) {
   let result = "";
 
   if (playerMove === "scissors") {
-    if (computerGuess === "rock") {
-      result = "You lose";
-    } else if (computerGuess === "paper") {
-      result = "You win";
-    } else if (computerGuess === "scissors") {
-      result = "Tie";
-    }
+    if (computerGuess === "rock") result = "You lose";
+    else if (computerGuess === "paper") result = "You win";
+    else result = "Tie";
   } else if (playerMove === "paper") {
-    if (computerGuess === "rock") {
-      result = "You win";
-    } else if (computerGuess === "paper") {
-      result = "Tie";
-    } else if (computerGuess === "scissors") {
-      result = "You lose";
-    }
-  } else if (playerMove === "rock") {
-    if (computerGuess === "rock") {
-      result = "Tie";
-    } else if (computerGuess === "paper") {
-      result = "You lose";
-    } else if (computerGuess === "scissors") {
-      result = "You win";
-    }
+    if (computerGuess === "rock") result = "You win";
+    else if (computerGuess === "scissors") result = "You lose";
+    else result = "Tie";
+  } else {
+    if (computerGuess === "paper") result = "You lose";
+    else if (computerGuess === "scissors") result = "You win";
+    else result = "Tie";
   }
 
-  if (result === "You win") {
-    score.wins += 1;
-  } else if (result === "You lose") {
-    score.losses += 1;
-  } else if (result === "Tie") {
-    score.ties += 1;
-  }
+  if (result === "You win") score.wins++;
+  else if (result === "You lose") score.losses++;
+  else score.ties++;
 
   localStorage.setItem("score", JSON.stringify(score));
 
-  document.querySelector(".js-result").innerHTML = result;
-  document.querySelector(
-    ".js-moves"
-  ).innerHTML = ` You <img class="move-img" src="images/${playerMove}-emoji.png" alt="" />
-      <img class="move-img" src="images/${computerGuess}-emoji.png" alt="" /> computer`;
+  const resultEl = document.querySelector(".js-result");
+  resultEl.innerHTML = result;
+
+  // remove old classes
+  resultEl.classList.remove("win", "lose", "tie");
+
+  if (result === "You win") resultEl.classList.add("win");
+  else if (result === "You lose") resultEl.classList.add("lose");
+  else resultEl.classList.add("tie");
+
+  // moves display
+  const movesEl = document.querySelector(".js-moves");
+  movesEl.innerHTML = `
+    You
+    <img class="move-img animate" src="images/${playerMove}-emoji.png">
+    <img class="move-img animate" src="images/${computerGuess}-emoji.png">
+    Computer
+  `;
+
   updateScore();
 }
 
 function updateScore() {
-  document.querySelector(
-    ".js-score"
-  ).innerHTML = `wins: ${score.wins}, losses: ${score.losses}, Ties: ${score.ties}`;
+  document.querySelector(".js-score").innerHTML =
+    `wins: ${score.wins}, losses: ${score.losses}, ties: ${score.ties}`;
+}
+
+function resetScore() {
+  score = { wins: 0, losses: 0, ties: 0 };
+  localStorage.removeItem("score");
+  updateScore();
 }
 
 function pickComputerMove() {
   const randomNumber = Math.random();
-  let computerGuess = "";
-  if (randomNumber >= 0 && randomNumber < 1 / 3) {
-    computerGuess = "rock";
-  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
-    computerGuess = "paper";
-  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
-    computerGuess = "scissors";
-  }
 
-  return computerGuess;
+  if (randomNumber < 1 / 3) return "rock";
+  if (randomNumber < 2 / 3) return "paper";
+  return "scissors";
 }
